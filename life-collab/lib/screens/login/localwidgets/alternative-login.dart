7190de21d7e8db602234cb0b76a62d83c7377242
Joflesan <s2088605@ed.ac.uk>
2021-08-06 16:49:50 +0100

    Google Login

--- life-collab/lib/screens/login/localwidgets/alternative-login.dart
+++ life-collab/lib/screens/login/localwidgets/alternative-login.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:life_collab/resources/menus/values/app_dimens.dart';
 import 'package:life_collab/resources/menus/values/app_styles.dart';
+import 'package:life_collab/screens/no-group/no-group.dart';
+import 'package:life_collab/states/currentUser.dart';
 import 'package:life_collab/widgets/link-account-button.dart';
+import 'package:provider/provider.dart';
 
 class AlternativeLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    void googleSignIn() {}
+    void googleLogin() async {
+      CurrentUser _currentUser =
+          Provider.of<CurrentUser>(context, listen: false);
+
+      try {
+        String _returnString = await _currentUser.loginUserWithGoogle();
+        if (_returnString == "success") {
+          Navigator.popUntil(
+              context, ModalRoute.withName('/')); // Pop all previous screens
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => NoGroupScreen()));
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(_returnString), duration: Duration(seconds: 2)));
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
 
     void facebookSignIn() {}
 
     var GoogleButton = LinkAccountButton(
-        click: googleSignIn,
+        click: googleLogin,
         text: "google",
         icon: Image.asset("assets/images/google.png"));
 
--- life-collab/lib/states/currentUser.dart
+++ life-collab/lib/states/currentUser.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 
 class CurrentUser extends ChangeNotifier {
@@ -52,6 +53,33 @@ class CurrentUser extends ChangeNotifier {
             content: Text("User Does Not Exist"),
             duration: Duration(seconds: 2)));
       }
+    } catch (e) {
+      retVal = e.message;
+    }
+
+    return retVal;
+  }
+
+  Future<String> loginUserWithGoogle() async {
+    String retVal = "error";
+
+    GoogleSignIn _googleSignIn = GoogleSignIn(scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ]);
+
+    try {
+      GoogleSignInAccount _googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication _googleAuth = await _googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: _googleAuth.idToken, accessToken: _googleAuth.accessToken);
+
+      final UserCredential _authResult =
+          await _auth.signInWithCredential(credential);
+
+      _uid = _authResult.user.uid;
+      _email = _authResult.user.email;
+      retVal = "success";
     } catch (e) {
       retVal = e.message;
     }
